a607b3bbb31712a634cac79287e2bf4c1dd18bc5
redhunt <sarthak613singh@gmail.com>
2024-08-18 23:33:28 +0530

    add deploy script and connecting web3 to web2

---
+++ web3Backened/script/DeployMarketPlace.s.sol
@@ -0,0 +1,14 @@
+// SPDX=License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import {Script} from "forge-std/Script.sol";
+import {FreelanceMarketplace} from "../src/MarketPlace.sol";
+
+contract Deploy is Script {
+    function run() external returns (FreelanceMarketplace) {
+        vm.startBroadcast();
+        FreelanceMarketplace freelanceMarketplace = new FreelanceMarketplace();
+        vm.stopBroadcast();
+        return freelanceMarketplace;
+    }
+}
