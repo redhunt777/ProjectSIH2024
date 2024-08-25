7f0b2e2d273fbce97b6f4fd1297b62c005575715
Shaurya Aditya Verma <shauryaaditya99@gmail.com>
2024-08-25 15:45:13 +0530

    refactor menu

--- FrontEnd/src/components/navbar/Navbar.jsx
+++ FrontEnd/src/components/navbar/Navbar.jsx
@@ -86,7 +86,33 @@ const Navbar = () => {
         <>
           <hr />
           <div className="menu">
-            
+          <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
           </div>
           <hr />
         </>
